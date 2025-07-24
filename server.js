730d82430b5a5ed6747abfaecae9f6fa41be15d1
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-24 11:27:43 +0200

    Added uncommited changes

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const mangoose = require('mangoose');
+const mongoose = require('mongoose');
 const app = express();
 
-
-
-
-app.listen(3000, ()=> {
-    console.log(`🚀 Server running at http://localhost:${port}`);
+mongoose.connect("mongodb+srv://adminOlivier:rQ4FZyFiqlAYIpmJ@cluster0.n95dhb1.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0")
+.then(()=>{
+    console.log("connected to the database");
+    app.listen(3000, ()=> {
+    console.log(`🚀 Server running at port 3000`);
     
 });
+})
+.catch(()=>{
+    console.log("Database connection failed");
+    
+})
 
 app.get('/', (req,res) => {
     res.send("Hello from the node API Server updated")
