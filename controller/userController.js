84751c5458331be108f409b49fce2753defde427
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-26 08:55:33 +0200

    Added comment for basic changes from Sequlize to Mongoose

---
+++ controller/userController.js
@@ -0,0 +1,5 @@
+// NOTES CHANGES FROM SEQUELIZE TO MONGOOSE
+// findAll()    ----> find()
+// findByPk(id) ----> findById(id)
+// update()     ----> findByIdAndUpdate(id)
+// destroy()    ----> findByIdAndDelete(id)
