4537d668796414f61c51e5cc9e89d965754c13e1
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-26 09:05:26 +0200

    Added create and Read user contoller function

--- controller/userController.js
+++ controller/userController.js
@@ -3,3 +3,30 @@
 // findByPk(id) ----> findById(id)
 // update()     ----> findByIdAndUpdate(id)
 // destroy()    ----> findByIdAndDelete(id)
+
+
+const userModel = require('../models/user');
+
+
+
+//create user
+exports.createUser = async (req,res) => {
+    const {fullName, email, password} = req.body
+    try {
+        const user = await userModel.create({fullName, email, password});
+        res.json({ message: "User created", user });
+    } catch (error) {
+       console.error('Error occurred:', error);
+       res.status(500).json({ error: 'Internal server error' }); 
+    }
+}
+
+// Read all user
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await userModel.find(); // Mongoose equivalent of findAll
+    res.json({ message: "List of all users", users });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
