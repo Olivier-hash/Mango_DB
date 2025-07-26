487ebf44113cc104fd5b6642f8eaa2ac6306fceb
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-26 08:46:44 +0200

    Added properties to the user Model with mongoose

--- models/user.js
+++ models/user.js
@@ -1,10 +1,24 @@
-const mongoose = require('mongoose')
-
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    age: Number
-})
+  fullName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true // adds createdAt and updatedAt fields automatically
+});
 
-module.exports = mongoose.model('USer', userSchema);
+module.exports = mongoose.model('User', userSchema);
