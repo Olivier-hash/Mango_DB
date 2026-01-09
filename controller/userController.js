f9a768383316b6cf3f05b937c22627db9367d1fd
olivier Rukundo <rr.olivier01@gmail.com>
2026-01-09 22:08:50 +0100

    implemented updateUser with findByIdAndUpdate

--- controller/userController.js
+++ controller/userController.js
@@ -61,7 +61,7 @@ exports.updateUser = async (req, res) => {
 
 
 
-// Delete user dat
+// Delete user da
 exports.deleteUser = async (req, res) => {
   try {
     const user = await userModel.findByIdAndDelete(req.params.id);
