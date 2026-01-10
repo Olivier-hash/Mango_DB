2abbaaf03312e2c087b4550bc946066724ef621c
olivier Rukundo <rr.olivier01@gmail.com>
2026-01-10 12:19:56 +0100

    implement deleteUser with findByIdAndDelete

--- controller/userController.js
+++ controller/userController.js
@@ -61,7 +61,7 @@ exports.updateUser = async (req, res) => {
 
 
 
-// Delete user da
+// Delete user 
 exports.deleteUser = async (req, res) => {
   try {
     const user = await userModel.findByIdAndDelete(req.params.id);
