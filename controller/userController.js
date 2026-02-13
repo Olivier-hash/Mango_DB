7bf1df7fcdb5ee87665250964eb96544ba1b9221
olivier Rukundo <rr.olivier01@gmail.com>
2026-02-13 23:45:01 +0100

    migrated to mongoose

--- controller/userController.js
+++ controller/userController.js
@@ -21,7 +21,7 @@ exports.createUser = async (req,res) => {
     }
 }
 
-// Read all user
+// Read all user  data
 exports.getAllUsers = async (req, res) => {
   try {
     const users = await userModel.find(); // Mongoose equivalent of findAll
