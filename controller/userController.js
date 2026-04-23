b9a5d5680b26bc47659ba0a2557375479d15a39e
olivier Rukundo <rr.olivier01@gmail.com>
2026-04-23 20:05:57 +0200

    added endpoint to retrieve all users from database

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to comm
+//time to comm start form commit 169
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
