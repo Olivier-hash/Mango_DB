cae61911e4e9be79216d4368a3405d445f71dfd7
olivier Rukundo <rr.olivier01@gmail.com>
2026-04-24 06:14:09 +0200

    handled missing user case with 404 response

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to comm start form commit 172
+//time to comm start form commit 172 edit
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
