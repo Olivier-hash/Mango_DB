9a4c11d1f0611886c4b217d298d27d8c1fad1474
olivier Rukundo <rr.olivier01@gmail.com>
2026-04-25 23:51:59 +0200

    improved error handling consistency across functions

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to comm start form commit 172 edit
+//time to comm start form commit 172 edi
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
