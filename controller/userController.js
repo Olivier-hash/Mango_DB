3376fe498efff3aa34642807be6953131159136c
olivier Rukundo <rr.olivier01@gmail.com>
2026-04-28 18:32:08 +0200

    returned proper response after successful deletion

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to committing start form commit 176 edit 
+//time to committin start form commit 176 edit 
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
