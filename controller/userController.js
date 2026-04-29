d099ce8cb8a9160e854fe72e2ba09081995d8707
olivier Rukundo <rr.olivier01@gmail.com>
2026-04-29 21:22:45 +0200

    used userModel consistently across all controllers

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to committi start form commit 180 edit cons
+//time to committi start form commit 180 edit con
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
