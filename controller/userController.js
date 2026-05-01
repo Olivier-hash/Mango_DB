8613a86cba180196d5050b6d263d1ec90d1aeef6
olivier Rukundo <rr.olivier01@gmail.com>
2026-05-01 23:38:49 +0200

    optimized Mongoose queries for better performance

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to committi start form commit 187 edit conne
+//time to committi start form commit 190 edit conne
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
