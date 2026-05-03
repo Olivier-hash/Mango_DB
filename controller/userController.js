644e373a849c128910dda1d1846398f8cd6e3135
olivier Rukundo <rr.olivier01@gmail.com>
2026-05-03 22:10:53 +0200

    improved error message readability and structure

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to committi start form commit 190 edit con
+//time to committi start form commit 190 edit co
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
