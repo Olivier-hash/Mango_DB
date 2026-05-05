7079e265b7ddee9a89e91dd9b6f94fa0166c83f7
olivier Rukundo <rr.olivier01@gmail.com>
2026-05-05 21:01:24 +0200

    hashed user password before saving to database

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to committi start form commit 196 editab
+//time to committi start form commit 196 edita
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
