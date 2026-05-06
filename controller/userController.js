41be4bbb6a00648d6e12c2c836caefb6f10c8df7
olivier Rukundo <rr.olivier01@gmail.com>
2026-05-06 22:07:23 +0200

    defined validation rules inside Mongoose schema

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to committi start form commit 196 edit
+//time to committi start form commit 199 edi
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
