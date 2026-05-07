12e0d65a5a82ebe89f6a616ebd238e48c8b57888
olivier Rukundo <rr.olivier01@gmail.com>
2026-05-07 22:38:27 +0200

    added structured logging for API requests

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to committi start form commit 205 externa
+//time to committi start form commit 205 extern
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
