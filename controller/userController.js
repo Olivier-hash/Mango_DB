b1306bb686af40cea22f91e76e675adc99f1a84b
olivier Rukundo <rr.olivier01@gmail.com>
2026-05-07 22:37:30 +0200

    sanitized incoming user input to prevent injection attacks

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to committi start form commit 205 external
+//time to committi start form commit 205 externa
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
