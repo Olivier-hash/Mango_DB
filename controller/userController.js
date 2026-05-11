5bcacfc8a12ca25c1fa649f3328ee810e6820b13
olivier Rukundo <rr.olivier01@gmail.com>
2026-05-11 22:20:31 +0200

    implemented global error handling middleware

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to committ start form commit 205 ext
+//time to committ start form commit 208 ext
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
