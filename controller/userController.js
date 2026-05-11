080bb9eac8423b757cdcd521ab1e4b05c061069d
olivier Rukundo <rr.olivier01@gmail.com>
2026-05-11 06:38:02 +0200

    create helper function for consistent API responses

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to committ start form commit 205 exter
+//time to committ start form commit 205 exte
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
