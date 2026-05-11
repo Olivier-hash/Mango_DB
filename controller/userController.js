42e36aedcad0d32bb773e452a5df8d0a28adc288
olivier Rukundo <rr.olivier01@gmail.com>
2026-05-11 06:37:24 +0200

    centralized logging utility for reuse

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to committi start form commit 205 extern
+//time to committ start form commit 205 exter
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
