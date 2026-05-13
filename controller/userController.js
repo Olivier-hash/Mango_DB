73d1f2fad42797014bce97e3e327e3da4bc82a94
olivier Rukundo <rr.olivier01@gmail.com>
2026-05-13 22:41:00 +0200

    connected controller functions to Express routes

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to committ start form commit 210 ex
+//time to committ start form commit 210 exit
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
