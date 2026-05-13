5509d2accc2317d2733e1332a78324a210d4c00d
olivier Rukundo <rr.olivier01@gmail.com>
2026-05-13 22:45:47 +0200

    moved business logic out of controllers

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to committ start form commit 214 exi
+//time to committ start form commit 214 ex
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
