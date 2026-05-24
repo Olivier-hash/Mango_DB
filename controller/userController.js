9ef4158573d0bc398e6b75fc7ec7c23c11aa5246
olivier Rukundo <rr.olivier01@gmail.com>
2026-05-24 22:14:52 +0200

    documented API endpoints, authentication, and architecture

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to commit start form commit 227
+//time to commit start form commit 231
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
