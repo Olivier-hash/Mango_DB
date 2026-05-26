2b0875774e0be0f1898a54056400444d1953d46e
olivier Rukundo <rr.olivier01@gmail.com>
2026-05-26 21:04:31 +0200

    added test coverage for error handling scenarios

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to commit start form commit 237 text
+//time to commit start form commit 237 tex
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
