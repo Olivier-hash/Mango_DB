4794e7eacfeaa647795665d137ebf65a6fd6ccd9
olivier Rukundo <rr.olivier01@gmail.com>
2026-05-29 06:36:20 +0200

    included timestamps in server logs

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to commit start form commit 241 testin
+//time to commit start form commit 241 testi
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
