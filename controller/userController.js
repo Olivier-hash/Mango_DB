85a841f4ac2ae34599054a32490fa84c09102905
olivier Rukundo <rr.olivier01@gmail.com>
2026-05-30 22:00:04 +0200

    reduced redundant database calls in controllers

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to commit start form commit 244 tes
+//time to commit start form commit 244 te
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
