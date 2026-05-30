204665e56c85ba6492b0eddc9814be18574b6117
olivier Rukundo <rr.olivier01@gmail.com>
2026-05-30 21:57:49 +0200

    optimized database queries with indexing strategy

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to commit start form commit 244 test
+//time to commit start form commit 244 tes
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
