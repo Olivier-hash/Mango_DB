5dfa734b93fbc21685259227e43b58719a8464e8
olivier Rukundo <rr.olivier01@gmail.com>
2026-06-05 18:05:09 +0200

    updated README with full architecture overview

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to commit start form commit 254 txasdo
+//time to commit start form commit 254 txasd
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
