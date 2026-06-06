f3cbe95b16dee5e4ea1707573bbbf0298000a57a
olivier Rukundo <rr.olivier01@gmail.com>
2026-06-06 21:53:38 +0200

    added centralized configuration for API base URL

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to commit start form commit 258 txa
+//time to commit start form commit 258 tx
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
