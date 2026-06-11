9a0c4b7eca28078a47062f126049fc70707465ea
olivier Rukundo <rr.olivier01@gmail.com>
2026-06-11 22:54:43 +0200

    enabled trimming of whitespace for fullName and email fields

--- models/user.js
+++ models/user.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
     required: true
   }
 }, {
-  timestamps: true // adds createdAt and updatedAt fields automatically
+  timestamps: true // adds, createdAt and updatedAt fields automatically
 });
 
 module.exports = mongoose.model('User', userSchema);
