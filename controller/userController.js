456d87e53124397460dd622e86246473073bfacd
olivier Rukundo <rr.olivier01@gmail.com>
2026-06-11 22:51:25 +0200

    added middleware to log request method and URL

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@
 const userModel = require('../models/user');
 
 
-//time to commit start form commit 266 te
+//time to commit start form commit 266 texting
 //create user
 exports.createUser = async (req,res) => {
     const {fullName, email, password} = req.body
