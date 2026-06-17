0c89a429a5942059afd6f2d8a6266c3213b4d080
olivier Rukundo <rr.olivier01@gmail.com>
2026-06-17 21:22:06 +0200

    registered User model with Mongoose

--- models/user.js
+++ models/user.js
@@ -23,4 +23,4 @@ const userSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('User', userSchema);
 
-    //  extra commit star from 278    lined
+    //  extra commit star from 278    line
