9d09668915bfc1e1e62076a0075cdb1227d78408
olivier Rukundo <rr.olivier01@gmail.com>
2026-06-19 07:28:27 +0200

    added comments explaining schema configuration

--- models/user.js
+++ models/user.js
@@ -23,4 +23,4 @@ const userSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('User', userSchema);
 
-    //  extra commit star from 281    line
+    //  extra commit star from 281    line (code)
