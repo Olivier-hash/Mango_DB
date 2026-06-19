c791e5e13d0d59abd12de1985ac8ecc16b28d7bf
olivier Rukundo <rr.olivier01@gmail.com>
2026-06-19 07:29:51 +0200

    finalized User model implementation for CRUD operations

--- models/user.js
+++ models/user.js
@@ -23,4 +23,4 @@ const userSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('User', userSchema);
 
-    //  extra commit star from 281    line (cod)
+    //  extra commit star from 281    line (co)
