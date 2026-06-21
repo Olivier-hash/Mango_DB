c167bc41013ab7cb33a71a51cbc71947ff6e3e02
olivier Rukundo <rr.olivier01@gmail.com>
2026-06-21 10:48:04 +0200

    improved field organization and naming consistency

--- models/user.js
+++ models/user.js
@@ -23,4 +23,4 @@ const userSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('User', userSchema);
 
-    //  extra commit star from 289    line ()
+    //  extra commit star from 289    lin()
