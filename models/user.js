81805d5c05a1591d1cb32ddde61ab321f997c639
olivier Rukundo <rr.olivier01@gmail.com>
2026-06-22 22:22:34 +0200

    separated schema definition from model registration

--- models/user.js
+++ models/user.js
@@ -23,4 +23,4 @@ const userSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('User', userSchema);
 
-    //  extra commit star from 289    lixicons(
+    //  extra commit star from 292    lixicons(
