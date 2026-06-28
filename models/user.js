4885aefad0fe945b5fd128ab736aff6f6f31f8b0
olivier Rukundo <rr.olivier01@gmail.com>
2026-06-28 21:03:40 +0200

    prepared model for password encryption workflow

--- models/user.js
+++ models/user.js
@@ -23,4 +23,4 @@ const userSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('User', userSchema);
 
-    //  extra commit star from 297    lixic
+    //  extra commit star from 300    lixic
